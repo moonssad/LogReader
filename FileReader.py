713bc6cd3c39634a75e0cb18118fbd9f8f2a7b4c
moonssad <wang.yizhe@byd.com>
2020-03-19 17:28:48 +0800

    第一版上传，实现基本功能，数据保存到data目录下。

---
+++ FileReader.py
@@ -0,0 +1,81 @@
+import os
+import time
+from datetime import datetime
+from text import detect_decoding_errors_line
+import re
+import csv
+import  copy
+import pandas as pd
+
+detail_list = list()
+detail_dict = dict()
+
+
+def read_file(filename):
+    global detail_list
+    global detail_dict
+    with open(filename, 'r', errors="surrogateescape") as f:
+        try:
+            for line in f:
+                errors = detect_decoding_errors_line(line)
+                if errors:
+                    pass
+                elif line.__contains__('RecorderPresenterImpl'):
+                    # 语音结束', '识别结果', '结束时间', '识别时间', '时间差
+                    if line.__contains__('RecorderPresenterImpl: 检测到语音结束点，正在进行识别处理,不需要再写入数据'):
+                        pattern = "\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d{1,3}"
+                        result = re.search(pattern, line)
+                        time = str_2_time(result[0])
+                        detail_dict['start'] = line
+                        detail_dict['start_time'] = time
+                    if line.__contains__('RecorderPresenterImpl: ---识别结果------'):
+                        pattern = "\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d{1,3}"
+                        result = re.search(pattern, line)
+                        time = str_2_time(result[0])
+                        detail_dict['end'] = line
+                        detail_dict['end_time'] = time
+                        if detail_dict.get('start_time') is not None:
+                            time_difference = time - detail_dict.get('start_time')
+                            detail_dict["time_difference"] = time_difference
+                            deatail  =copy.deepcopy(detail_dict)
+                            detail_list.append(deatail)
+                            # print(deatail)
+                            detail_dict.clear()
+
+
+        except UnicodeDecodeError:
+            print('当前分母为0')
+
+
+def str_2_time(str):
+    real_time = "2020-{}".format(str)
+    datetime_obj = datetime.strptime(real_time, "%Y-%m-%d %H:%M:%S.%f")
+    obj_stamp = int(time.mktime(datetime_obj.timetuple()) * 1000.0 + datetime_obj.microsecond / 1000.0)
+    return obj_stamp
+
+
+#
+def read_dir(rootdir):
+    list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
+    for i in range(0, len(list)):
+        path = os.path.join(rootdir, list[i])
+        if os.path.isfile(path):
+            read_file(path)
+    save_csv(detail_list, 'tongji_all')
+    # save_csv_pd(detail_list,'tongji_pd.csv')
+
+
+def save_csv(data, filename):
+    with open(filename + ".csv", 'w', encoding="utf-8") as f:
+        headers = ['start', 'start_time', 'end', 'end_time', 'time_difference']
+        writers = csv.DictWriter(f, headers)
+        writers.writeheader()
+        for date in data:
+            print(date)
+            writers.writerow(date)
+        # data.clear()
+
+def save_csv_pd(data, filename):
+    pd.DataFrame(data).to_csv(filename)
+read_dir("data/")
+
